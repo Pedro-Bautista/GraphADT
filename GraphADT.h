9569e5ca1d2c08ebe2c632ed80e4533cec8a5b5d
kusf <kennethverzyl@usf.edu>
2023-04-16 00:30:48 -0400

    Commit me

--- GraphADT.h
+++ GraphADT.h
@@ -4,14 +4,19 @@
 
 #ifndef GRAPHADT_GRAPHADT_H
 #define GRAPHADT_GRAPHADT_H
+
+#include <list>
+
 template<typename G>
 class ObjectVertex{
+public:
     G operator*();
     G incidentEdges();
     G isAdjacentTo(V);
 };
 template<typename G>
 class ObjectEdge{
+public:
     G operator*()
     endVertices();
     opposite(V);
@@ -21,11 +26,18 @@ class ObjectEdge{
 };
 template<typename G>
 class GraphADT{
-    vertices()
-    edges()
-    insertVertex(x)
-    insertEdge(v,w,x)
-    eraseVertex(v)
-    eraseEdge(e)
+public:
+    std::list<ObjectVertex<G>> vertices();
+    std::list<ObjectEdge<G>> edges();
+    void insertVertex(x);
+    void insertEdge(ObjectVertex<G>,ObjectVertex<G>,ObjectEdge<G>);
+    void eraseVertex(ObjectVertex<G>);
+    void eraseEdge(e);
 };
+
+template<typename G>
+void GraphADT<G>::eraseVertex(ObjectVertex<G>) {
+
+}
+
 #endif //GRAPHADT_GRAPHADT_H
