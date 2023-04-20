8c61ee3be5e32bb3c474b3d4ceb0a1c718300e62
kusf <130384593+kusf@users.noreply.github.com>
2023-04-20 12:15:55 -0400

    Update GraphADT.h

--- GraphADT.h
+++ GraphADT.h
@@ -48,12 +48,12 @@ public:
 template<typename G>
 bool GraphADT<G>::isAdjacent(ObjectVertex<G> A, ObjectVertex<G> B) {
     //std::cout << "\n" << *A << " tested with " << *B << std::endl;
-    std::__1::__list_iterator<ObjectEdge<std::__1::basic_string<char>>, void *> i = edgesList.begin();
+    std::list<ObjectEdge<std::basic_string<char>>>::iterator i = edgesList.begin();
     for (; i != edgesList.end(); ++i) {
         if (i->isAdjacentTo(A)) {
             //std::cout << *(*i->incidentList.begin());
-            std::__1::list<ObjectVertex<std::__1::basic_string<char>>> list2 = i.operator*().incidentList;
-            std::__1::list<ObjectVertex<std::__1::basic_string<char>>>::iterator k;
+            std::list<ObjectVertex<std::basic_string<char>>> list2 = i.operator*().incidentList;
+            std::list<ObjectVertex<std::basic_string<char>>>::iterator k;
             for (k = list2.begin(); k != list2.end(); ++k) {
                 if (*B == **k) {
                     //std::cout << *A << " == " << *B << std::endl;
@@ -78,7 +78,7 @@ ObjectVertex<G> GraphADT<G>::insertVertex(ObjectVertex<G> vertex) {
 template<typename G>
 void GraphADT<G>::eraseVertex(ObjectVertex<G> V) {
 
-    std::list<std::__1::__list_iterator<ObjectEdge<std::__1::basic_string<char>>, void *>> toBeRemoved;
+    std::list<std::list<ObjectEdge<std::basic_string<char>>>::iterator> toBeRemoved;
     //loops edgeList and stores any iterators that link to edges to be removed in toBeRemoved list
     for (auto j = edgesList.begin(); j != edgesList.end(); ++j) {
         if (j->isAdjacentTo(V)) {
